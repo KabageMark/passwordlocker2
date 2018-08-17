105e17b05d17dfaf4dda78d7995ae362ee343ba1
kabagemark <kabagemark@gmail.com>
2018-08-17 11:04:18 +0300

    creating credential and saving account functions created

--- password.py
+++ password.py
@@ -4,3 +4,10 @@ from dataclass import Passwords
 def createaccount(firstname,lastname,email):
     new_account = Account(firstname,lastname,email)
     return new_account
+    
+def saveaccount(account):
+     account.saveaccount()    
+
+def createcredential(credentialname,password,email):
+    new_account2 = Passwords(credentialname,password,email)
+    return new_account2
