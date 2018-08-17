9be42c502a2d0c775ff15470009f03110c703ba2
kabagemark <kabagemark@gmail.com>
2018-08-17 10:56:13 +0300

    Passwords data class created

--- dataclass.py
+++ dataclass.py
@@ -7,3 +7,12 @@ class Account:
 
     def saveaccount(self):
         Account.account_list.append(self)
+
+class Passwords:
+    password_list = []
+    def __init__(self,password,email,credentialtype):
+        self.password = password
+        self.email = email
+
+    def savepassword(self):
+        Passwords.password_list.append(self)        
